46b43f14e26b330f53d3d5ce6ebe9f6a200d94e5
Romas <hello@codesomelabs.com>
2021-09-18 18:14:11 +0300

    About block

---
+++ web/app/themes/isto/resources/views/blocks/about-block.blade.php
@@ -0,0 +1,36 @@
+{{--
+  Title: About
+  Category: design
+  Icon: schedule
+  Mode: edit
+  Align: left
+  PostTypes: page post
+  SupportsAlign: false
+  SupportsMode: false
+  SupportsMultiple: true
+--}}
+
+@php
+    $verticalSpace = get_field('vertical_space') ?? true ? 'my-70px' : '';
+    $title = get_field('title');
+	$text = get_field('text');
+	$imageId = get_field('image');
+@endphp
+
+<div class="block-about {{ $verticalSpace }}">
+    <div class="container">
+        @if(!empty($title))
+            <h2 class="text-35px text-center mb-8">
+                {{ $title }}
+            </h2>
+        @endif
+        <div class="text-14px text-center mt-8 mb-20">
+            {!! $text !!}
+        </div>
+        @if(!empty($imageId))
+            <div>
+                <img src="{{ wp_get_attachment_image_url($imageId, 'xlarge') }}" alt="">
+            </div>
+        @endif
+    </div>
+</div>
--- web/app/themes/isto/resources/views/blocks/history-block.blade.php
+++ web/app/themes/isto/resources/views/blocks/history-block.blade.php
@@ -32,7 +32,7 @@
                     x-cloak="showMore"
                     x-show="showMore"
                     x-transition:enter="transition ease-out duration-1000"
-                    x-transition:enter-start="opacity-0 transform scale-90" 
+                    x-transition:enter-start="opacity-0 transform scale-90"
                     x-transition:enter-end="opacity-100 transform scale-100"
                     x-transition:leave="transition ease-in duration-1000"
                     x-transition:leave-start="opacity-100 transform scale-100"
