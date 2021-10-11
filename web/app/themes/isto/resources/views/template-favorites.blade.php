137ad76ea62424627b47725913e078a87b839cbb
Romas <hello@codesomelabs.com>
2021-10-11 21:29:12 +0300

    Favorites page improvements

--- web/app/themes/isto/resources/views/template-favorites.blade.php
+++ web/app/themes/isto/resources/views/template-favorites.blade.php
@@ -11,7 +11,17 @@
             <?php the_content(); ?>
 
             @if(empty($product_ids))
-                <div class="text-center">{{ __('Mėgstamiausių produktų neturite', 'isto') }}</div>
+                <div class="text-center">
+                    <div>{{ __('Mėgstamiausių produktų neturite', 'isto') }}</div>
+                    <div>{{ __('Pažymėkite dominančius produktus.', 'isto') }}</div>
+                    <div>{{ __('Tiesiog spustelėkite ant širdelės simbolio ir jis bus rodomas čia.', 'isto') }}</div>
+                    <div>
+                        <a href="{{ \App\getPermalinkByTemplate('product-categories') }}" class="with-arrow text-14px border border-black px-[55px] py-4 inline-flex items-center space-x-1 mt-8">
+                            <span>{{ __('Produktų katalogas', 'isto') }}</span>
+                            <img src="@asset('images/arrow-right-1.svg')" class="arrow-right" alt="">
+                        </a>
+                    </div>
+                </div>
             @else
                 <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
                     @foreach($product_ids as $productId)
