2e5cd9c0eceda6ac4f8c5957ad40c089fbd58410
Romas <hello@codesomelabs.com>
2021-10-11 17:51:12 +0300

    Products block changes

--- web/app/themes/isto/resources/views/blocks/products-block.blade.php
+++ web/app/themes/isto/resources/views/blocks/products-block.blade.php
@@ -13,27 +13,32 @@
 @php
     $verticalSpace = get_field('vertical_space') ?? true ? 'my-80px' : '';
     $title = get_field('global')['title'];
-    $categories = get_field('product_categories');
+	$blocks = get_field('blocks');
 @endphp
 
-<div class="block-products {{ $verticalSpace }}">
-    <div class="container">
-        @include('blocks.partials.title')
+@if($blocks)
+    <div class="block-products {{ $verticalSpace }}">
+        <div class="container">
+            @include('blocks.partials.title')
 
-        <div class="grid lg:grid-cols-2 gap-16 lg:h-[574px]">
-            @foreach ($categories as $category)
-            @php
-                $categoryImage = wp_get_attachment_image_url( get_term_meta($category->term_id, 'thumbnail_id', true), 'large' );
-            @endphp
-                <a href="#" class="group block bg-cover bg-center"
-                    style="background-image: url({{ $categoryImage }})">
-                    <h3 class="bg-white transition-opacity opacity-80 group-hover:opacity-100 w-[255px] h-[238px] text-center text-22px leading-tight flex items-center justify-center p-14">
-                        {{ $category->name }}
-                    </h3>
-                </a>
-            @endforeach
+            <div class="grid lg:grid-cols-2 gap-16 lg:h-[574px]">
+                @foreach ($blocks as $block)
+                    @php
+                        $image = wp_get_attachment_image_url( $block['image'], 'xlarge' );
+                        $link = $block['link'];
+                    @endphp
+                    <a href="{{ $link['url'] }}" class="group block bg-cover bg-center"
+                        style="background-image: url({{ $image }})">
+                        @if(!empty($link['title']))
+                            <h3 class="bg-white transition-opacity opacity-80 group-hover:opacity-100 w-[255px] h-[238px] text-center text-22px leading-tight flex items-center justify-center p-14">
+                                {{ $link['title'] }}
+                            </h3>
+                        @endif
+                    </a>
+                @endforeach
+            </div>
+
+            @include('blocks.partials.more-link', ['title' => __('Visi produktai', 'isto'), 'link' => \App\getPermalinkByTemplate('product-categories') ])
         </div>
-
-        @include('blocks.partials.more-link', ['title' => __('Visi produktai', 'isto'), 'link' => get_permalink(wc_get_page_id('shop')) ])
     </div>
-</div>
+@endif
