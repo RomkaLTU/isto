19809991beb1bb8cd49c51b10b15f0db0b368ec3
RD <hello@codesomelabs.com>
2021-10-14 17:46:01 +0300

    Inspiration block fixes

--- web/app/themes/isto/resources/views/blocks/inspiration-block.blade.php
+++ web/app/themes/isto/resources/views/blocks/inspiration-block.blade.php
@@ -14,10 +14,11 @@
 $verticalSpace = get_field('vertical_space') ?? true ? 'my-80px' : '';
 $title = get_field('global')['title'];
 $imageId = get_field('image');
-$object = \Illuminate\Support\Arr::first(get_field('object'));
-$objectImage = wp_get_attachment_image_url(get_post_thumbnail_id($object->ID), 'xlarge');
-$contentClean = wp_strip_all_tags( $object->post_content );
-$excerpt = \Illuminate\Support\Str::words($contentClean, 40, '...');
+$group = get_field('group');
+$groupImageId = $group['image'];
+$groupTitle = $group['title'];
+$groupText = $group['text'];
+$groupLink = $group['link'];
 @endphp
 
 <div class="block-inspiration {{ $verticalSpace }}">
@@ -33,16 +34,20 @@ $excerpt = \Illuminate\Support\Str::words($contentClean, 40, '...');
             <div class="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-3">
               <div class="lg:col-start-2 col-span-2">
                 <div class="text-14px lg:pl-40px">
-                    <h3 class="text-22px mb-5">{{ $object->post_title }}</h3>
-                    <div class="mb-5 max-w-[607px]">{{ $excerpt }}</div>
+                    @if(!empty($groupTitle))
+                        <h3 class="text-22px mb-5">{{ $groupTitle }}</h3>
+                    @endif
+                    @if(!empty($groupText))
+                        <div class="mb-5 max-w-[607px]">{{ $groupText }}</div>
+                    @endif
 
-                    <a href="#" class="with-arrow text-14px border border-black px-[55px] py-4 inline-flex items-center space-x-1">
+                    <a href="{{ $groupLink['url'] }}" class="with-arrow text-14px border border-black px-[55px] py-4 inline-flex items-center space-x-1">
                         <span>{{ __('Daugiau info', 'isto') }}</span>
                         <img src="@asset('images/arrow-right-1.svg')" class="arrow-right" alt="">
                     </a>
-                    @if (!empty($objectImage))
+                    @if (!empty($groupImageId))
                         <div class="text-center">
-                            <img src="{{ $objectImage }}" class="mx-auto max-h-[320px]" alt="">
+                            <img src="{{ wp_get_attachment_image_url( $groupImageId, 'xlarge' ) }}" class="mx-auto max-h-[320px]" alt="">
                         </div>
                     @endif
                 </div>
