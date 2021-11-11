a9b7afbcf9a02cf44d65924f30c6aa9c238ea76c
Romas <r.daskevicius@gmail.com>
2021-11-11 23:53:53 +0200

    file upload fix

--- web/import.php
+++ web/import.php
@@ -12,18 +12,20 @@
 
 <?php
 if ($_POST) {
-	$target_dir = "app/uploads/wpallimport/files/";
+	$target_dir = "../app/uploads/wpallimport/files/";
 	$target_file = $target_dir . 'Isto.xlsx';
 	$fileTYpe = strtolower(pathinfo($target_dir . basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION));
 
 	if($fileTYpe !== "xlsx") {
 		echo "Netinkamas failo formatas!";
+		exit();
 	}
 
 	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 		echo "Failas patalpintas.";
 	} else {
 		echo "Sorry, there was an error uploading your file.";
+		exit();
 	}
 }
 ?>
