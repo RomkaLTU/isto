a4284c2289a3a4938671373237fdc8196ecbfaa7
Romas <hello@codesomelabs.com>
2022-01-11 10:47:54 +0200

    Additional lang switcher shortciode check.

--- web/app/themes/isto/resources/views/partials/header.blade.php
+++ web/app/themes/isto/resources/views/partials/header.blade.php
@@ -19,7 +19,9 @@
               </g>
             </svg>
           </a>
-          <?php echo do_shortcode('[weglot_switcher]') ?>
+          @if(shortcode_exists('weglot_switcher'))
+            <?php echo do_shortcode('[weglot_switcher]') ?>
+          @endif
         </div>
       </div>
       <div class="lg:hidden mt-[7px]">
