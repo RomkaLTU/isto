7ed715768884bbb6f865328e2485a83bb1ef18c8
RD <hello@codesomelabs.com>
2022-02-05 11:53:05 +0200

    Bugfixes, pagination align changed to right.

--- web/app/themes/isto/resources/assets/scripts/app.js
+++ web/app/themes/isto/resources/assets/scripts/app.js
@@ -19,18 +19,18 @@ window.Alpine = Alpine;
 // ------------------------
 
 new SwiperCore('.swiper-container-products', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 32,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 64,
     },
-    slidesPerView: 1,
-    spaceBetween: 32,
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 64,
-        },
-    }
+  }
 });
 
 // end Products slider
@@ -40,18 +40,18 @@ new SwiperCore('.swiper-container-products', {
 // ------------------------
 
 new SwiperCore('.swiper-container-manufacturers, .swiper-container-related', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 8,
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 16,
     },
-    slidesPerView: 1,
-    spaceBetween: 8,
-    breakpoints: {
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 16,
-        },
-    }
+  }
 });
 
 // end Manufacturers slider
@@ -60,58 +60,39 @@ new SwiperCore('.swiper-container-manufacturers, .swiper-container-related', {
 // Hero slider
 // ------------------------
 
+const heroSlideAnimation = function(swiper) {
+  const tl = anime.timeline({
+    easing: 'easeOutSine',
+  });
+
+  const line = document.querySelector(`.swiper-slide-active [data-hero-line]`);
+  const text = document.querySelector(`.swiper-slide-active [data-hero-text]`);
+
+  tl
+    .add({
+      targets: line,
+      top: 0,
+      duration: 1700,
+    })
+    .add({
+      targets: text,
+      opacity: 1,
+      duration: 1700,
+    });
+}
+
 new SwiperCore('.swiper-container-hero', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 8,
-    on: {
-        init: function (swiper) {
-            const tl = anime.timeline({
-                easing: 'easeOutSine',
-            });
-
-            const line = document.querySelector(`[data-hero-line="${swiper.activeIndex}"]`);
-            const text = document.querySelector(`[data-hero-text="${swiper.activeIndex}"]`);
-            const slide = document.querySelector(`[data-swiper-slide="${swiper.activeIndex}"]`);
-
-            tl
-                .add({
-                    targets: line,
-                    top: 0,
-                    duration: 1700,
-                })
-                .add({
-                    targets: text,
-                    opacity: 1,
-                    duration: 1700,
-                });
-        },
-        slideNextTransitionStart: function(swiper) {
-            const tl = anime.timeline({
-                easing: 'easeOutSine',
-            });
-
-            const line = document.querySelector(`[data-hero-line="${swiper.activeIndex}"]`);
-            const text = document.querySelector(`[data-hero-text="${swiper.activeIndex}"]`);
-            const slide = document.querySelector(`[data-swiper-slide="${swiper.activeIndex}"]`);
-
-            tl
-                .add({
-                    targets: line,
-                    top: 0,
-                    duration: 1700,
-                })
-                .add({
-                    targets: text,
-                    opacity: 1,
-                    duration: 1700,
-                });
-        },
-    },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 8,
+  on: {
+    slideNextTransitionStart: heroSlideAnimation,
+    slidePrevTransitionStart: heroSlideAnimation,
+  },
 });
 
 // end Hero slider
@@ -121,45 +102,45 @@ new SwiperCore('.swiper-container-hero', {
 // ------------------------
 
 new SwiperCore('.swiper-container-materials, .swiper-container-simple', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-    },
-    slidesPerView: 1,
-    spaceBetween: 8,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  slidesPerView: 1,
+  spaceBetween: 8,
 });
 
 // end Materials slider
 
 document.querySelectorAll('.scrollmagic').forEach(function(el) {
-    const controller = new ScrollMagic.Controller();
-    const tl = anime.timeline({
-        easing: 'easeOutSine',
+  const controller = new ScrollMagic.Controller();
+  const tl = anime.timeline({
+    easing: 'easeOutSine',
+  });
+
+  const lineDown = el.querySelector('.anime-line-down');
+  const fadeFromLeft = el.querySelector('.anime-fade-from-left');
+  const fadeIn = el.querySelector('.anime-fade-in');
+
+  new ScrollMagic.Scene({ triggerElement: el, reverse: false, triggerHook: 100 })
+    .addTo(controller)
+    .on("enter", function () {
+      tl.add({
+        targets: lineDown,
+        height: '150%',
+      }).add({
+        targets: fadeFromLeft,
+        opacity: 1,
+        translateX: ['-100%', '0'],
+      }).add({
+        targets: fadeIn,
+        opacity: 1,
+      });
     });
-
-    const lineDown = el.querySelector('.anime-line-down');
-    const fadeFromLeft = el.querySelector('.anime-fade-from-left');
-    const fadeIn = el.querySelector('.anime-fade-in');
-
-    new ScrollMagic.Scene({ triggerElement: el, reverse: false, triggerHook: 100 })
-        .addTo(controller)
-        .on("enter", function () {
-            tl.add({
-                targets: lineDown,
-                height: '150%',
-            }).add({
-                targets: fadeFromLeft,
-                opacity: 1,
-                translateX: ['-100%', '0'],
-            }).add({
-                targets: fadeIn,
-                opacity: 1,
-            });
-        });
 });
 
 // ------------------------
@@ -167,103 +148,103 @@ document.querySelectorAll('.scrollmagic').forEach(function(el) {
 // ------------------------
 
 document.addEventListener('alpine:init', () => {
-    Alpine.data('contactUs', () => ({
-        data: {
-            loading: false,
-            referer: null,
-            type: null,
-            products: [],
-            name: null,
-            email: null,
-            phone: null,
-            message: null,
-            city: null,
-            privacy_policy: false,
-            productId: null,
-            errors: false,
-            response: null,
-            sent: false,
-        },
+  Alpine.data('contactUs', () => ({
+    data: {
+      loading: false,
+      referer: null,
+      type: null,
+      products: [],
+      name: null,
+      email: null,
+      phone: null,
+      message: null,
+      city: null,
+      privacy_policy: false,
+      productId: null,
+      errors: false,
+      response: null,
+      sent: false,
+    },
 
-        setProps(productId, products, referer, contactType) {
-            this.data.productId = productId;
-            this.data.products = products ? JSON.parse(products) : [];
-            this.data.referer = referer;
-            this.data.type = contactType;
-        },
+    setProps(productId, products, referer, contactType) {
+      this.data.productId = productId;
+      this.data.products = products ? JSON.parse(products) : [];
+      this.data.referer = referer;
+      this.data.type = contactType;
+    },
 
-        handleErrors(response) {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
+    handleErrors(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
 
-            return response;
-        },
+      return response;
+    },
 
-        handleValidation() {
-            if (
-                !this.data.name ||
-                !this.data.email ||
-                !this.data.phone ||
-                !this.data.message ||
-                !this.data.city ||
-                !this.data.privacy_policy
-            ) {
-                this.data.errors = true;
-                this.data.sent = false;
-                this.data.loading = false;
-                this.data.response = 'Fields market with * is required.';
+    handleValidation() {
+      if (
+        !this.data.name ||
+        !this.data.email ||
+        !this.data.phone ||
+        !this.data.message ||
+        !this.data.city ||
+        !this.data.privacy_policy
+      ) {
+        this.data.errors = true;
+        this.data.sent = false;
+        this.data.loading = false;
+        this.data.response = 'Fields market with * is required.';
 
-                return false;
-            }
+        return false;
+      }
 
-            return true;
-        },
+      return true;
+    },
 
-        resetForm() {
-            this.data.name = null;
-            this.data.type = null;
-            this.data.referer = null;
-            this.data.products = [];
-            this.data.productId = false;
-            this.data.email = null;
-            this.data.phone = null;
-            this.data.message = null;
-            this.data.city = null;
-            this.data.privacy_policy = false;
-        },
+    resetForm() {
+      this.data.name = null;
+      this.data.type = null;
+      this.data.referer = null;
+      this.data.products = [];
+      this.data.productId = false;
+      this.data.email = null;
+      this.data.phone = null;
+      this.data.message = null;
+      this.data.city = null;
+      this.data.privacy_policy = false;
+    },
 
-        submitForm() {
-            this.data.loading = true;
-            this.data.errors = false;
+    submitForm() {
+      this.data.loading = true;
+      this.data.errors = false;
 
-            if (!this.handleValidation()) {
-                return;
-            }
+      if (!this.handleValidation()) {
+        return;
+      }
 
-            fetch('/wp-json/isto/v1/contactus', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(this.data),
-            })
-            .then(this.handleErrors)
-            .then(() => {
-                this.data.errors = false;
-                this.data.loading = false;
-                this.data.response = '';
-                this.data.sent = true;
-                this.resetForm();
-            })
-            .catch((error) => {
-                this.data.errors = true;
-                this.data.loading = false;
-                this.data.response = error;
-            })
-            .finally(() => {
-                this.loading = false;
-            });
-        },
-    }));
+      fetch('/wp-json/isto/v1/contactus', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.data),
+      })
+        .then(this.handleErrors)
+        .then(() => {
+          this.data.errors = false;
+          this.data.loading = false;
+          this.data.response = '';
+          this.data.sent = true;
+          this.resetForm();
+        })
+        .catch((error) => {
+          this.data.errors = true;
+          this.data.loading = false;
+          this.data.response = error;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  }));
 });
 
 // end Contact us form
@@ -281,7 +262,7 @@ categoriesTitleEl.innerText = 'Produktų katalogas';
 categoriesTitleEl.classList.add('uppercase','text-13px','mb-2', 'inline-block');
 
 if (blockProductCategories) {
-    blockProductCategories.before(categoriesTitleEl);
+  blockProductCategories.before(categoriesTitleEl);
 }
 
 const filtersTitleEl = document.createElement('div');
@@ -290,27 +271,27 @@ filtersTitleEl.innerText = 'Filtruoti';
 filtersTitleEl.classList.add('uppercase','text-13px','mb-1', 'inline-block', 'border-t', 'border-gray-2', 'pt-5');
 
 if (sidebarFilters) {
-    document.querySelector('.sidebar-filters').before(filtersTitleEl);
+  document.querySelector('.sidebar-filters').before(filtersTitleEl);
 }
 
 parentCategories.forEach(function(el) {
-    const parentCategory = el.querySelector('a');
-    const arrowEl = document.createElement('span');
+  const parentCategory = el.querySelector('a');
+  const arrowEl = document.createElement('span');
 
-    arrowEl.classList.add('sidebar-sections-toggle');
+  arrowEl.classList.add('sidebar-sections-toggle');
 
-    parentCategory.before(arrowEl);
+  parentCategory.before(arrowEl);
 
-    arrowEl
-        .addEventListener('click', function(e) {
-            e.preventDefault();
+  arrowEl
+    .addEventListener('click', function(e) {
+      e.preventDefault();
 
-            const parentItem = e.target;
-            const submenu = parentItem.nextElementSibling.nextElementSibling;
+      const parentItem = e.target;
+      const submenu = parentItem.nextElementSibling.nextElementSibling;
 
-            parentItem.classList.toggle('inactive');
-            submenu.classList.toggle('hidden');
-        });
+      parentItem.classList.toggle('inactive');
+      submenu.classList.toggle('hidden');
+    });
 
 
 });
@@ -318,13 +299,13 @@ parentCategories.forEach(function(el) {
 const bapfAttributeTitle = document.querySelectorAll('.bapf_head');
 
 bapfAttributeTitle.forEach(function(el) {
-    // @FIXME po filtravimo nebeveikia click event'as
-    el.addEventListener('click', function(e) {
-        e.preventDefault();
+  // @FIXME po filtravimo nebeveikia click event'as
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
 
-        el.classList.toggle('inactive');
-        el.nextElementSibling.classList.toggle('hidden');
-    });
+    el.classList.toggle('inactive');
+    el.nextElementSibling.classList.toggle('hidden');
+  });
 });
 
 // end Product categories
@@ -336,33 +317,33 @@ bapfAttributeTitle.forEach(function(el) {
 const parentItems = document.querySelectorAll('.nav-primary-mobile > .menu-item-has-children > .sub-menu > li.menu-item > a');
 
 parentItems.forEach(function(parentItem) {
-    parentItem.addEventListener('click', function(el) {
-        el.preventDefault();
+  parentItem.addEventListener('click', function(el) {
+    el.preventDefault();
 
-        const parentItemTitle = el.currentTarget.innerText;
-        const subMenu = el.currentTarget.nextElementSibling;
-        const subMenuTitleEl = document.createElement('li');
-        const subMenuTitleBackEl = document.createElement('img');
-        const subMenuTitleContent = document.createTextNode(parentItemTitle);
+    const parentItemTitle = el.currentTarget.innerText;
+    const subMenu = el.currentTarget.nextElementSibling;
+    const subMenuTitleEl = document.createElement('li');
+    const subMenuTitleBackEl = document.createElement('img');
+    const subMenuTitleContent = document.createTextNode(parentItemTitle);
 
-        subMenu.classList.add('active');
+    subMenu.classList.add('active');
 
-        subMenuTitleEl.classList.add('submenu-title');
-        subMenuTitleEl.appendChild(subMenuTitleContent);
+    subMenuTitleEl.classList.add('submenu-title');
+    subMenuTitleEl.appendChild(subMenuTitleContent);
 
-        subMenuTitleBackEl.setAttribute('src', '/arrow-left.svg');
-        subMenuTitleBackEl.classList.add('submenu-back', 'w-6');
+    subMenuTitleBackEl.setAttribute('src', '/arrow-left.svg');
+    subMenuTitleBackEl.classList.add('submenu-back', 'w-6');
 
-        subMenuTitleEl.prepend(subMenuTitleBackEl);
-        subMenu.prepend(subMenuTitleEl);
+    subMenuTitleEl.prepend(subMenuTitleBackEl);
+    subMenu.prepend(subMenuTitleEl);
 
-        subMenu.querySelector('.submenu-back').addEventListener('click', function(e) {
-            e.preventDefault();
+    subMenu.querySelector('.submenu-back').addEventListener('click', function(e) {
+      e.preventDefault();
 
-            subMenuTitleEl.remove();
-            subMenu.classList.remove('active');
-        });
+      subMenuTitleEl.remove();
+      subMenu.classList.remove('active');
     });
+  });
 });
 
 // end Product categories mobile
@@ -372,39 +353,39 @@ parentItems.forEach(function(parentItem) {
 // --------------------------
 
 if (document.querySelector('.inner-scroll')) {
-    new PerfectScrollbar('.inner-scroll', {
-        wheelSpeed: 2,
-        wheelPropagation: true,
-        minScrollbarLength: 20
-    });
+  new PerfectScrollbar('.inner-scroll', {
+    wheelSpeed: 2,
+    wheelPropagation: true,
+    minScrollbarLength: 20
+  });
 }
 
 // end Perfect scroll init
 
 Alpine.store('favs', {
-    items: [],
+  items: [],
 
-    init() {
-        const itemsCookie = Cookies.get('favs');
+  init() {
+    const itemsCookie = Cookies.get('favs');
 
-        if (itemsCookie) {
-            this.items = JSON.parse(itemsCookie);
-        }
-    },
-
-    toggleFav(productId) {
-        if (!this.items.includes(productId)) {
-            this.items.push(productId);
-        } else {
-            this.items = this.items.filter((item) => item !== productId);
-        }
-
-        Cookies.set('favs', JSON.stringify(this.items));
-    },
-
-    isSelected(productId) {
-        return this.items.includes(productId);
+    if (itemsCookie) {
+      this.items = JSON.parse(itemsCookie);
     }
+  },
+
+  toggleFav(productId) {
+    if (!this.items.includes(productId)) {
+      this.items.push(productId);
+    } else {
+      this.items = this.items.filter((item) => item !== productId);
+    }
+
+    Cookies.set('favs', JSON.stringify(this.items));
+  },
+
+  isSelected(productId) {
+    return this.items.includes(productId);
+  }
 });
 
 Alpine.plugin(persist);
--- web/app/themes/isto/resources/views/blocks/hero-slider-block.blade.php
+++ web/app/themes/isto/resources/views/blocks/hero-slider-block.blade.php
@@ -23,10 +23,10 @@
                     @php
                         $bgImage = wp_get_attachment_image_url($slide['image'], 'xlarge');
                     @endphp
-                    <div data-swiper-slide="{{ $loop->iteration - 1 }}" class="swiper-slide group block bg-no-repeat bg-cover bg-center"
+                    <div data-swiper-slide class="swiper-slide group block bg-no-repeat bg-cover bg-center"
                         style="background-image: url({{ $bgImage }})">
-                        <div data-hero-line="{{ $loop->iteration - 1 }}" class="hero-line bg-white w-1px h-[150px] lg:h-[300px] absolute -top-full right-0 mr-[50%] lg:mr-[38%]">
-                            <div data-hero-text="{{ $loop->iteration - 1 }}" class="hero-text opacity-0 relative h-full">
+                        <div data-hero-line class="hero-line bg-white w-1px h-[150px] lg:h-[300px] absolute -top-full right-0 mr-[50%] lg:mr-[38%]">
+                            <div data-hero-text class="hero-text opacity-0 relative h-full">
                                 <div class="absolute bottom-0 left-0 -mb-32 lg:-ml-1 uppercase w-[320px] -translate-x-1/2 lg:transform-none text-center lg:text-left text-white text-50px leading-tight font-headings font-bold">
                                     {{ $slide['title'] }}
                                 </div>
